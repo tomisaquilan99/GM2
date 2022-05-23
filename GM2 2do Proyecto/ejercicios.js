d7986a7095c7bbfe0fd1f9119b35a98551e8f9b6
tomisaquilan99 <tomas.saquilan@gm2dev.com>
2022-05-23 14:29:02 -0300

    "Nuevo commit"

--- GM2 2do Proyecto/ejercicios.js
+++ GM2 2do Proyecto/ejercicios.js
@@ -1,18 +1,61 @@
 const resultados = document.getElementById("resultados");
-const input2Container = document.getElementById("input-2");
-const input2 = document.createElement("input");
-const input2Instrucciones = document.createElement("p");
+const inputAdd = document.getElementById("input-2");
+const listaEjercicios = document.getElementById("ejercicios")
+const infoEjercicio = document.getElementById("ej-info")
+const infoInput2 = document.getElementById("ej1-input2-info")
+
+function generalidadesCases234 (textarea,input2,info1,info2){
+        textarea.textContent = ""
+        input2.style.display ="none";
+        info1.style.display = "block";
+        info2.style.display = "none";
+}
+
+const opcionCambiada =() =>{
+    const opcionSeleccionada = listaEjercicios.selectedIndex;
+    switch (opcionSeleccionada){
+        case 1:
+            resultados.textContent = ""
+            inputAdd.style.display = "block";
+            infoEjercicio.style.display ="block";
+            infoInput2.style.display = "block"
+            infoEjercicio.innerHTML = "Ingrese el array a sumar y restar";
+            infoInput2.innerHTML = "Ingrese el número a sumar y restar"
+            break;
+            
+        case 2:
+            generalidadesCases234(resultados,inputAdd,infoEjercicio,infoInput2);
+            infoEjercicio.innerHTML = "Ingrese el array del cual desea saber si hay al menos un número par y paridad";
+            break;
+
+        case 3:
+            generalidadesCases234(resultados,inputAdd,infoEjercicio,infoInput2);
+            infoEjercicio.innerHTML = "Ingrese la palabra la cual desea saber si es palíndromo o podría serlo";
+            break;
+
+        case 4:
+            generalidadesCases234(resultados,inputAdd,infoEjercicio,infoInput2);
+            infoEjercicio.innerHTML = "Ingrese el número que desea convertir a romano";
+            break;
+    }
+}
 
 const formLogic= (event) => {
     event.preventDefault()
+    infoEjercicio.style.display ="none";
+    infoInput2.style.display ="none";
     let form = event.target;
     let el=form.elements;
     let input = el["content"].value;
     let exercise = el["ejercicios"].value;
+    let input2 = el["content2"].value;
 
     switch(exercise){
         case "suma-resta":
             const arraySumaResta = transformInputToNumbersArray(input);
+            const res01 = adicion(arraySumaResta, Number(input2));
+            const res02 = resta(arraySumaResta, Number(input2));
+            resultados.textContent = "La suma es " + res01 + " y la resta es " + res02;
             break;
 
         case "par-pareja": 
@@ -31,14 +74,16 @@ const formLogic= (event) => {
         case "numero-romano":
             const numRomano = input;
             const res4 = convertirARomano(numRomano)
-            resultados.textContent = res4;
+            resultados.textContent = "El numero convertido en romano es " + res4;
             break;
     }
+    inputAdd.style.display = "none";
     form.reset();
 }
 
 const form = document.getElementById("form-ejs");
 form.addEventListener('submit', formLogic);
+listaEjercicios.addEventListener("change", opcionCambiada);
 
 function transformInputToNumbersArray (input){
     const arrayStrings = input.split(",");
@@ -58,7 +103,10 @@ function adicion(array, numero){
         arrayToNumber += num.toString();
     }
 
-    arrayToNumber = (Number(arrayToNumber)+numero).toString().split("").map(elem=>Number(elem));
+    arrayToNumber = (Number(arrayToNumber)+numero);
+    console.log(arrayToNumber);
+    arrayToNumber = arrayToNumber.toString().split("").map(elem=>Number(elem));
+    console.log(arrayToNumber);
 
     return arrayToNumber
 }
